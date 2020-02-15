6301c3177841c001c576ea20e8ef91d0292e18eb
Jeff Tian <jeff.tian@outlook.com>
2020-02-15 18:35:49 +0800

    feat: http trigger

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ module.exports.initializer = function(context, callback) {
 };
 */
 
-export const handler = function (event, context, callback) {
-    console.log('hello world, from typescript');
-    callback(null, "hello world, from typescript");
+export const handler = function (request, response, context) {
+  console.log('hello world, from typescript');
+
+  response.setStatusCode(200);
+  response.send(JSON.stringify({
+    message: 'hello world, from typescript',
+    ...request.queries,
+  }));
 };
--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,12 +1,10 @@
 import { handler } from '../src'
 import assert = require('assert');
 
-const promisify = (callbackway: any) => async (...args: any) => new Promise((resolve, reject) => callbackway(...args, (err, res) => err ? reject(err) : resolve(res)))
-
 describe('hello', () => {
     it('greets', async () => {
-        const res = await promisify(handler)({}, {})
+        handler({ queries: { a: 'a' } }, { setStatusCode: () => { }, send: console.log }, {})
 
-        assert(res === 'hello world, from typescript')
+        assert(process.title === 'npm')
     })
 })
